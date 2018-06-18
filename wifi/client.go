df50794af8c9b1561ec1ecbeb7c47c061b8e6054
andygeiss <4ndygeiss@gmail.com>
2018-06-18 19:40:52 +0200

    switched from bool to int

--- wifi/client.go
+++ wifi/client.go
@@ -7,6 +7,6 @@ type Client struct {}
 // The return value indicates success or failure.
 // connect() also supports DNS lookups when using a domain name (ex:google.com).
 // @see: https://www.arduino.cc/en/Reference/WiFiClientConnect
-func (c *Client) Connect(host string, port int) bool {
-	return true
+func (c *Client) Connect(host string, port int) int {
+	return StatusConnected
 }
