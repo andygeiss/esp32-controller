4bf283a5d69862f36e0604ce8c12c190fbfefea1
andygeiss <4ndygeiss@gmail.com>
2018-08-02 21:33:38 +0200

    client write added

--- wifi/client.go
+++ wifi/client.go
@@ -1,7 +1,9 @@
 package wifi
 
+import "fmt"
+
 // Client ...
-type Client struct {}
+type Client struct{}
 
 // Connect to the IP address and port specified in the constructor.
 // The return value indicates success or failure.
@@ -9,4 +11,13 @@ type Client struct {}
 // @see: https://www.arduino.cc/en/Reference/WiFiClientConnect
 func (c *Client) Connect(host string, port int) int {
 	return StatusConnected
+}
+
+// Write data to the server the client is connected to.
+// Return the number of characters written.
+// It is not necessary to read this value.
+// @see: https://www.arduino.cc/en/Reference/WiFiClientWrite
+func (c *Client) Write(data interface{}) int {
+	fmt.Print(data)
+	return 0
 }
