f5a63da0f6a2dafd0c03068dd0cbde64d4fa4edb
andygeiss <4ndygeiss@gmail.com>
2018-08-05 19:07:43 +0200

    HTTP Client demo finished & different WiFi status added

--- wifi/client.go
+++ wifi/client.go
@@ -9,8 +9,16 @@ type Client struct{}
 // The return value indicates success or failure.
 // connect() also supports DNS lookups when using a domain name (ex:google.com).
 // @see: https://www.arduino.cc/en/Reference/WiFiClientConnect
-func (c *Client) Connect(host string, port int) int {
-	return StatusConnected
+func (c *Client) Connect(host string, port int) bool {
+	return true
+}
+
+// Print data, followed by a carriage return and newline, to the server a client is connected to.
+// Prints numbers as a sequence of digits, each an ASCII character (e.g. the number 123 is sent as the three characters '1', '2', '3').
+// @see: https://www.arduino.cc/en/Reference/WiFiClientPrintln
+func (c *Client) Println(data interface{}) int {
+	fmt.Println(data)
+	return 0
 }
 
 // Write data to the server the client is connected to.
